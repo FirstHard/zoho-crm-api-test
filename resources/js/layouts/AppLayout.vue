c10636798142ed02e5c1a5f2b2da792c5d718b9f
Volodymyr Buinov <buinoff@gmail.com>
2024-04-21 18:36:32 +0300

    Installed and configured Vue.js framework, Bootstrap 5 library, created Vue components

---
+++ resources/js/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <FormComponent></FormComponent>
+</template>
+
+<script>
+import FormComponent from '../components/FormComponent.vue';
+export default {
+  name: "AppLayout",
+  components: {
+    FormComponent,
+  },
+};
+</script>
+<style lang="scss">
+</style>
